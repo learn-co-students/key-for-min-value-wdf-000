a57c35ecfc4177220568add3484427027171739c
Naveed Shah <naveed@tayybah.com>
2016-07-30 17:00:13 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,22 @@
 
 def key_for_min_value(name_hash)
 
+  i = 0
+  temp = []
+
+  name_hash.each do |key, value|
+    if i == 0
+      temp <<  value
+      temp << key
+      i += 1
+    end
+
+    if value < temp[0]
+      temp[0] = value
+      temp[1] = key
+    end
+  end
+
+  temp[1]
+
 end
